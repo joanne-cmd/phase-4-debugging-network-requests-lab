aa7e0d3d18e977d006e0f6fcb602d611ad929174
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-03-20 11:07:01 +0300

    completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,8 +7,10 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
+  rescue ActiveRecord::RecordInvalid=>invalid
+    render json:{errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
   end
 
   def update
@@ -16,10 +18,25 @@ class ToysController < ApplicationController
     toy.update(toy_params)
   end
 
+  def increment_likes
+    toy= Toy.find_by(id: params[:id])
+    if toy
+      toy.update(likes: toy.likes + 1)
+      render json: toy
+    else
+      render json: { error: "toy not found" }, status: :not_found
+    end
+  end
+  
   def destroy
     toy = Toy.find_by(id: params[:id])
+    if toy
     toy.destroy
     head :no_content
+    else
+      render json: {error: "toys not found"}, status: :not_found
+    end
+
   end
 
   private
